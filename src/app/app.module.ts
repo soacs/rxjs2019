6ccf47926295ecb17816582d377e03f694a6496e
LAPTOP-JHLSCDA7\steve <steve@soaconsultingservices.com>
2020-04-25 11:44:49 -0400

    upgrade angular 9 and replaced app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/application/app.component';
+import { SubjectsComponent } from './components/subjects/subjects.component';
+import { OperatorsComponent } from './components/operators/operators.component';
+import { SchedulerComponent } from './components/scheduler/scheduler.component';
+import { BehaviorComponent } from './components/behavior/behavior.component';
+import { AsyncComponent } from './components/async/async.component';
+import { ReplayComponent } from './components/replay/replay.component';
+import { ObserversComponent } from './components/observers/observers.component';
+import { CompletionsComponent } from './components/completions/completions.component';
+import { MulticastComponent } from './components/multicast/multicast.component';
+import { UtilityComponent } from './components/utility/utility.component';
+import { CreationComponent } from './components/creation/creation.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { TransformationsComponent } from './components/transformations/transformations.component';
+import { ConditionalsComponent } from './components/conditionals/conditionals.component';
+import { CombinationsComponent } from './components/combinations/combinations.component';
+import { MathematicalComponent } from './components/mathematical/mathematical.component';
+import { ErrorhandlingComponent } from './components/errorhandling/errorhandling.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubjectsComponent,
+    OperatorsComponent,
+    BehaviorComponent,
+    AsyncComponent,
+    ReplayComponent,
+    ObserversComponent,
+    CompletionsComponent,
+    MulticastComponent,
+    UtilityComponent,
+    SchedulerComponent,
+    CreationComponent,
+    IntroComponent,
+    FiltersComponent,
+    TransformationsComponent,
+    ConditionalsComponent,
+    CombinationsComponent,
+    MathematicalComponent,
+    ErrorhandlingComponent
   ],
   imports: [
     BrowserModule
