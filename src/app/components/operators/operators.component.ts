44deef555c8eed7f1eaf95481bf536d42058d1cc
LAPTOP-JHLSCDA7\steve <steve@soaconsultingservices.com>
2020-05-17 11:10:36 -0400

    operators completed

--- src/app/components/operators/operators.component.ts
+++ src/app/components/operators/operators.component.ts
@@ -11,36 +11,55 @@ import * as Rx from 'rxjs';
 })
 export class OperatorsComponent implements OnInit, OnDestroy {
   subscription: Subscription;
-  observable: Observable<number>;
+  observable2: Observable<number>;
+
+  mergeItArray: Array<any> = [];
+  filterItArray: Array<number> = [];
+  errorItArray: Array<any> = [];
+  scanItArray: Array<number> = [];
+  pluckItArray: Array<any> = [];
+  windowItArray: Array<any> = [];
+  combineItArray: Array<any> = [];
 
   constructor() {
-  console.log('ENTER OperatorsComponent Constructor');
-  console.log('EXIT OperatorsComponent Constructtor');
+    console.log('ENTER OperatorsComponent Constructor');
+    console.log('EXIT OperatorsComponent Constructor');
   }
 
   ngOnInit() {
-   // this.mergeIt();
-    // this.filterIt();
-    // this.errorIt();
-    // this.scanIt();
-    // this.pluckIt();
-   // this.windowIt();
     this.combineIt();
+    this.mergeIt();
+    this.filterIt();
+    this.errorIt();
+    this.scanIt();
+    this.pluckIt();
+    this.windowIt();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   mergeIt() {
-    const observable1 = interval(5000, );
+    const observable1 = interval(5000);
     const observable2 = interval(2000);
-    const merged = merge(observable1, observable2).pipe(take(10));
-    this.subscription = merged.subscribe(x => console.log('merge: ' + x));
+    const merged = merge(observable1, observable2).pipe(take(3));
+    this.subscription = merged.subscribe(x => {
+      console.log('merge: ' + x);
+      this.mergeItArray.push(x);
+      }
+    );
   }
 
   filterIt() {
     const nums = of(1, 2, 3, 4, 5);
     const squareOddVals = pipe(filter((n: number) => n % 2 !== 0), map(n => n * n));
     const squareOdd = squareOddVals(nums);
-    squareOdd.subscribe(x => console.log('squareOddVals: ' + x));
-
+    squareOdd.subscribe(x => {
+        console.log('squareOddVals: ' + x);
+        this.filterItArray.push(x);
+      }
+    );
   }
 
   scanIt() {
@@ -48,64 +67,83 @@ export class OperatorsComponent implements OnInit, OnDestroy {
     const ones = clicks.pipe(mapTo(1));
     const seed = 0;
     const count = ones.pipe(scan((acc, one) => acc + one, seed));
-    count.subscribe(x => console.log('scan: ' + x));
+    count.subscribe(x => {
+        console.log('scan: ' + x);
+        this.scanItArray.push(x);
+      }
+    );
   }
 
   pluckIt() {
     const clicks = fromEvent(document, 'click');
     const tagNames = clicks.pipe(tap(x => console.log(x)), pluck('target', 'tagName'));
-    tagNames.subscribe(x => console.log('pluck: ' + x));
+    tagNames.subscribe(x => {
+        console.log('pluck: ' + x);
+        this.pluckItArray.push(x);
+      }
+    );
   }
 
   combineIt() {
+    const obs1 = of(1, 2, 3, 4);
+    const obs2 = of(5, 6, 7, 8);
+
     const observable = forkJoin(
-      of(1, 2, 3, 4),
-      of(5, 6, 7, 8)
+      {
+        obs1,
+        obs2
+      }
     );
     observable.subscribe(
-      value => console.log('forkJoin: ' + value),
-      err => {},
-      () => console.log('End of the forkJoin streams!')
+      value => {
+        console.log('forkJoin: ' + value);
+        this.combineItArray.push(value);
+      },
+      err => console.log('Error'),
+      () => console.log('End of the combine!')
+    );
+    const obs3 = interval(1000).pipe(take(3)); // emit 0, 1, 2 every second and complete
+    const obs4 = interval(500).pipe(take(4)); // emit 0, 1, 2, 3 every half a second and complete
+    const observable2 = forkJoin({
+        obs3, obs4
+      }
     );
 
-
-    const observable2 = forkJoin(
-      interval(1000).pipe(take(3)), // emit 0, 1, 2 every second and complete
-      interval(500).pipe(take(4)) // emit 0, 1, 2, 3 every half a second and complete
-    );
-    observable.subscribe(
-      value => console.log('forkJoin after: ' + value),
-      err => {},
+    observable2.subscribe(
+      value =>  {
+        console.log('forkJoin: ' + value);
+        this.combineItArray.push(value);
+      },
+      err => {
+      },
       () => console.log('End of the forkJoin after streams!')
     );
-
-
-
   }
 
   windowIt() {
-  const clicks = fromEvent(document, 'click');
-  const intervaler = interval(5000);
-  const result = clicks.pipe(window(intervaler), map(win => win.pipe(take(2)), mergeAll()));
-  result.subscribe(x => console.log('windowIt: ' + JSON.stringify(x)));
-}
+    const clicks = fromEvent(document, 'click');
+    const windowObs = interval(50000);
+    const result = clicks.pipe(window(windowObs), map(win => win.pipe(take(3)), mergeAll()));
+    result.subscribe(x => {
+        console.log('window: ' + x);
+        this.windowItArray.push(x);
+      }
+    );
+  }
 
   errorIt() {
     of(10, 20, 30, 40, 50).pipe(
       map(n => {
           if (n === 30) {
-            throw new Error('30 error!');
+            throw new Error('The value 30 is not allowed!');
           }
           return n;
         }
-      ), catchError(err => of('errorIt one', 'errorIt two'))).
-    subscribe(x => console.log(x));
+      ), catchError(err => of( err.toString()))).subscribe(x => {
+        console.log('error: ' + x);
+        this.errorItArray.push(x);
+      }
+    );
 
   }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-
-
 }
